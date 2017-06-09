57e78c0f53cfe733efffe1ed54723ae3d67f2e09
XiuYuLi <295553381@qq.com>
2017-06-09 18:33:13 +0800

    Create xfft_status.h

--- include/xfft_status.h
+++ include/xfft_status.h
@@ -1,12 +1,12 @@
 ﻿#ifndef __xfft_status_h__
 #define __xfft_status_h__
 
-#define XFFT_SUCCESS						          0x0
-#define XFFT_ERROR_INVALID_VALUE			    0x1
-#define XFFT_ERROR_INVALID_DRIVER			    0x2
-#define XFFT_ERROR_INVALID_DEVICE			    0x3
-#define XFFT_ERROR_OUT_OF_MEMORY			    0x4
-#define XFFT_ERROR_OUT_OF_DEVICE_MEMORY		0x5
+#define XFFT_SUCCESS                      0x0
+#define XFFT_ERROR_INVALID_VALUE          0x1
+#define XFFT_ERROR_INVALID_DRIVER         0x2
+#define XFFT_ERROR_INVALID_DEVICE         0x3
+#define XFFT_ERROR_OUT_OF_MEMORY          0x4
+#define XFFT_ERROR_OUT_OF_DEVICE_MEMORY   0x5
 #define XFFT_ERROR_OUT_MAX_SIZE_SUPPORTED	0x6
 
 #endif
